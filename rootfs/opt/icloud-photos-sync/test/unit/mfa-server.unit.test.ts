8f74115e3be9ea4aece08f8ac05cf4d5597e0346
Frank Steiler <frank@steilerdev.de>
2022-08-28 09:19:34 +0000

    Working on CI

--- rootfs/opt/icloud-photos-sync/test/unit/mfa-server.unit.test.ts
+++ rootfs/opt/icloud-photos-sync/test/unit/mfa-server.unit.test.ts
@@ -1,6 +1,9 @@
 
-import {describe, test} from '@jest/globals';
+import {expect, describe, test} from '@jest/globals';
 describe(`Unit Tests - MFA Server`, () => {
+    test(`Should fail`, () => {
+        expect(true).toEqual(false)
+    })
     test.todo(`Should start & stop`);
     test.todo(`Should be able to parse valid token`);
     test.todo(`Should reject invalid token`);
