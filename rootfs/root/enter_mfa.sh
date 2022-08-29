e68a1ed411f7ca87fb624d8fe66d7bc3ffc69050
Frank Steiler <frank@steilerdev.de>
2022-08-29 08:10:40 +0000

    Fixing some issues and setup

--- rootfs/root/enter_mfa.sh
+++ rootfs/root/enter_mfa.sh
@@ -2,4 +2,4 @@
 MFA_PORT="${PORT:-80}"
 HOST="${2:-localhost}"
 echo "Posting MFA code $1 to ${HOST}:${MFA_PORT}"
-curl -s -X POST ${HOST}:${MFA_PORT}/mfa?code=$1
+curl -s -X POST "${HOST}:${MFA_PORT}/mfa?code=$1"
