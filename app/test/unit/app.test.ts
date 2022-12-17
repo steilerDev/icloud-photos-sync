0da70ea780ea7fe4542a1b7b5f5f32529f3a2477
Frank Steiler <frank@steilerdev.de>
2022-12-17 17:54:47 +0000

    Defining open test cases

--- app/test/unit/app.test.ts
+++ app/test/unit/app.test.ts
@@ -7,4 +7,19 @@ describe(`Unit Tests - iCloud App`, () => {
         test.todo(`Create Sync App`)
         test.todo(`Create Archive App`)
     })
+    describe(`App control flow`, () => {
+        test.todo(`Handle authentication error`)
+        describe(`Token App`, () => {
+            test.todo(`Execute token actions`)
+            test.todo(`Handle trust token error`)
+        })
+        describe(`Sync App`, () => {
+            test.todo(`Execute sync actions`)
+            test.todo(`Handle sync error`)
+        })
+        describe(`Archive App`, () => {
+            test.todo(`Execute archive actions`)
+            test.todo(`Handle archive error`)
+        })
+    })
 })
