14d1c3f74b61d08e722adef65bc7030399ca6792
Frank Steiler <frank@steilerdev.de>
2023-02-27 14:52:30 +0100

    Linting

--- app/test/unit/icloud.auth.test.ts
+++ app/test/unit/icloud.auth.test.ts
@@ -1,5 +1,5 @@
 import mockfs from 'mock-fs';
-import * as fs from 'fs'
+import * as fs from 'fs';
 import {describe, test, expect, jest, afterEach} from "@jest/globals";
 import {getICloudCookies, iCloudAuthFactory, mockValidation} from "../_helpers/icloud-auth.helper";
 import * as Config from '../_helpers/_config';
@@ -16,14 +16,14 @@ describe(`Unit Tests - iCloud Auth`, () => {
                 '/opt': mockfs.directory({
                     "mode": 0o000,
                     "uid": 0,
-                    "gid": 0
+                    "gid": 0,
                 }),
             });
-            
+
             // For some reason required
-            fs.chownSync('/opt', 0, 0)
-            fs.chmodSync('/opt', 0o000)
-            
+            fs.chownSync(`/opt`, 0, 0);
+            fs.chmodSync(`/opt`, 0o000);
+
             const auth = iCloudAuthFactory();
             await expect(auth.storeTrustToken()).rejects.toThrowError(`Unable to store trust token`);
         });
