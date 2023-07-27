aea90830f652012e01cc130c6ee32fe3061a924c
Frank Steiler <frank@steilerdev.de>
2023-07-27 18:41:20 +0200

    Fixing schema build

--- app/build-schema.sh
+++ app/build-schema.sh
@@ -9,9 +9,19 @@ if [ ! -d "$TARGET_FOLDER" ]; then
   mkdir -p "$TARGET_FOLDER"
 fi
 
+# Ensuring the files are present and writeable and in order to avoid warnings when generating the schemas for the first time
+touch ${TARGET_FOLDER}/resource-file.json \
+    ${TARGET_FOLDER}/signin-response.json \
+    ${TARGET_FOLDER}/resend-mfa-device-response.json \
+    ${TARGET_FOLDER}/resend-mfa-phone-response.json \
+    ${TARGET_FOLDER}/trust-response.json \
+    ${TARGET_FOLDER}/setup-response.json \
+    ${TARGET_FOLDER}/photos-setup-response.json
+
 $CMD --type ResourceFile --path src/lib/resource-manager/resources.ts > ${TARGET_FOLDER}/resource-file.json
 $CMD --additional-properties true --type SigninResponse --path src/lib/resource-manager/network.ts > ${TARGET_FOLDER}/signin-response.json
 $CMD --additional-properties true --type ResendMFADeviceResponse --path src/lib/resource-manager/network.ts > ${TARGET_FOLDER}/resend-mfa-device-response.json
 $CMD --additional-properties true --type ResendMFAPhoneResponse --path src/lib/resource-manager/network.ts > ${TARGET_FOLDER}/resend-mfa-phone-response.json
 $CMD --additional-properties true --type TrustResponse --path src/lib/resource-manager/network.ts > ${TARGET_FOLDER}/trust-response.json
+$CMD --additional-properties true --type SetupResponse --path src/lib/resource-manager/network.ts > ${TARGET_FOLDER}/setup-response.json
 $CMD --additional-properties true --type PhotosSetupResponse --path src/lib/resource-manager/network.ts > ${TARGET_FOLDER}/photos-setup-response.json
