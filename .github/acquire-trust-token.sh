6d7caf43fcd508c73f89fa18a20167d359f21dfd
Frank Steiler <frank@steilerdev.de>
2025-02-13 16:17:00 +0100

    Fixing trust token acquisition script

--- .github/acquire-trust-token.sh
+++ .github/acquire-trust-token.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
-# # Settings for mapping the ICPS directory into the container
-# SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-# SRC_DIR=$(dirname $SCRIPT_DIR)
-# ICPS_DOCKER_DIR="/icloud-photos-sync"
-# ICPS_RESEND_MFA_SCRIPT="resend_mfa"
-# ICPS_ENTER_MFA_SCRIPT="enter_mfa"
-
-PROJECT_DIR="$(pwd)"
 TMP_DIR="$(mktemp -d)"
 
-ACTION_RUNNER_DIR="/opt/action-runner/"
-ACTION_RUNNER_SERVICE=""
+ACTION_RUNNER_DIR="/opt/actions-runner"
+ACTION_RUNNER_SERVICE="actions.runner.steilerDev-icloud-photos-sync.steilerGroup-HomeServer.service"
 ACTION_RUNNER_ENVIRONMENT="$ACTION_RUNNER_DIR/.env"
 
+# Getting current user/password
 source $ACTION_RUNNER_ENVIRONMENT
 
-# Settings for requesting the MFA Code
-ICPS_PORT=8080
+# Settings for requesting the MFA Code (this is account specific)
 MFA_METHOD="sms"
 MFA_ID="2"
 
@@ -35,38 +27,56 @@ case $(git rev-parse --abbrev-ref HEAD) in
     ;;
 esac
 
+DOCKER_IMAGE="steilerdev/icloud-photos-sync:$IMAGE_TAG"
+
+echo -n "Starting $DOCKER_IMAGE..."
+
 DOCKER_NAME=$(docker run -d \
   -e APPLE_ID_USER="$TEST_APPLE_ID_USER" \
   -e APPLE_ID_PWD="$TEST_APPLE_ID_PWD" \
-  -v $TMP_DIR:/icloud-photos-library \
-  steilerdev/icloud-photos-sync:$IMAGE_TAG
+  -v $TMP_DIR:/opt/icloud-photos-library \
+  $DOCKER_IMAGE \
   token
 )
+echo "container $DOCKER_NAME is running!"
 
-echo -n "Waiting for MFA server to become available on localhost, port $ICPS_PORT"
+echo -n "Waiting for MFA server to become available.."
 
-until nc -z localhost ${ICPS_PORT}; do
+until docker logs $DOCKER_NAME | grep -q "Listening for input on port"; do
     echo -n '.'
-    sleep 5
+    sleep 1
+    if docker logs $DOCKER_NAME | grep -q "Error"; then
+        echo "Error in container:"
+        docker logs $DOCKER_NAME
+        docker stop $DOCKER_NAME
+        rm -rf $TMP_DIR
+        exit 1
+    fi
 done
 echo "server available"
 
+echo "Requesting code..."
 # Resend MFA code via SMS
-docker exec $DOCKER_NAME $ICPS_RESEND_MFA_SCRIPT $MFA_METHOD $MFA_ID
+docker exec $DOCKER_NAME resend_mfa $MFA_METHOD $MFA_ID > /dev/null
 
-echo "Please enter MFA code"
+echo "Please enter MFA code:"
 read MFA_CODE
 
 # Send MFA code
-docker exec $DOCKER_NAME $ICPS_ENTER_MFA_SCRIPT $MFA_CODE
+docker exec $DOCKER_NAME enter_mfa $MFA_CODE > /dev/null
 
-NEW_TRUST_TOKEN="$(cat $TMP_DIR/.icloud_photos_sync | jq -r '.trust_token')"
+echo "Waiting for container to exit..."
+docker wait $DOCKER_NAME
+
+NEW_TRUST_TOKEN="$(cat $TMP_DIR/.icloud-photos-sync | jq -r '.trustToken')"
 
 > $ACTION_RUNNER_ENVIRONMENT
 echo "TEST_APPLE_ID_USER=$TEST_APPLE_ID_USER" >> $ACTION_RUNNER_ENVIRONMENT
 echo "TEST_APPLE_ID_PWD=$TEST_APPLE_ID_PWD" >> $ACTION_RUNNER_ENVIRONMENT
 echo "TEST_TRUST_TOKEN=$NEW_TRUST_TOKEN" >> $ACTION_RUNNER_ENVIRONMENT
 
+echo -n "Update variables, restarting action runner..."
 sudo systemctl restart $ACTION_RUNNER_SERVICE
 
-rm -r $TMP_DIR
+echo "done, cleaning up!"
+rm -rf $TMP_DIR
